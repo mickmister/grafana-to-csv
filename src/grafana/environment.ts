51f6f2c228f607c7f8d2d227994dc1c0f90f3a6f
mickmister <6913320+mickmister@users.noreply.github.com>
2024-02-15 03:36:59 -0500

    run all queries instead of just average time

--- src/grafana/environment.ts
+++ src/grafana/environment.ts
@@ -10,12 +10,6 @@ if (!GRAFANA_DATASOURCE_UID) {
     process.exit(0);
 }
 
-export const GRAFANA_DATASOURCE_ID = process.env.GRAFANA_DATASOURCE_ID!;
-if (!GRAFANA_DATASOURCE_ID) {
-    console.log('Please provide a datasource id via GRAFANA_DATASOURCE_ID env var');
-    process.exit(0);
-}
-
 export const GRAFANA_NAMESPACE = process.env.GRAFANA_NAMESPACE!;
 if (!GRAFANA_NAMESPACE) {
     console.log('Please provide a Grafana namespace via GRAFANA_NAMESPACE env var');
